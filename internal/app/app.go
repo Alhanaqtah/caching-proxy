054a292eaa89febf6da2814e0398b9b317c96c99
Banana Duck <a.usama@example.com>
2024-10-26 13:30:22 +0300

    cache

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,38 +2,48 @@ package app
 
 import (
 	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/Alhanaqtah/caching-proxy/internal/service"
 
 	"github.com/urfave/cli/v2"
 )
 
 func Run(ctx context.Context, args []string) error {
 	app := cli.NewApp()
-
 	app.Name = "caching-proxy"
 	app.Usage = "A proxy server for caching"
-	app.Description = "caching server that caches responses from other servers."
+	app.Description = "Caching server that caches responses from other servers."
+	app.UsageText = "caching-proxy [global options] command [command options]"
 
 	app.Commands = []*cli.Command{
 		{
 			Name:        "run",
 			Usage:       "Runs the server",
-			Description: "starts a proxy server for caching",
-			Action: func(ctx *cli.Context) error {
-				return nil
+			Description: "Starts a proxy server for caching",
+			Action: func(c *cli.Context) error {
+				port := c.String("port")
+				origin := c.String("origin")
+
+				if _, err := url.ParseRequestURI(origin); err != nil {
+					return fmt.Errorf("invalid origin URL: %v", err)
+				}
+
+				return service.Run(port, origin)
 			},
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:     "port",
 					Aliases:  []string{"p"},
-					Value:    "8080",
 					Required: true,
-					Usage:    "port on which the server will listen",
+					Usage:    "Port on which the server will listen",
 				},
 				&cli.StringFlag{
 					Name:     "origin",
 					Aliases:  []string{"o"},
 					Required: true,
-					Usage:    "origin server to cache responses from",
+					Usage:    "Origin server to cache responses from",
 				},
 			},
 		},
@@ -41,8 +51,8 @@ func Run(ctx context.Context, args []string) error {
 			Name:        "clear",
 			Aliases:     []string{"c"},
 			Usage:       "Removes all cached responses",
-			Description: "removes all cached responses",
-			Action: func(ctx *cli.Context) error {
+			Description: "Removes all cached responses",
+			Action: func(c *cli.Context) error {
 				return nil
 			},
 		},
---
+++ internal/service/service.go
@@ -0,0 +1,151 @@
+package service
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	cache = make(map[string]*cacheItem)
+	wg    sync.RWMutex
+)
+
+type cacheItem struct {
+	StatusCode int
+	Header     http.Header
+	Body       []byte
+}
+
+func Run(port, origin string) error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Validation
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Method " + r.Method + " not allowed\n"))
+			log.Println("unsupported method.")
+			return
+		}
+
+		t := time.Now()
+
+		// Search in cache
+		rKey := prepareCacheKey(r)
+		wg.RLock()
+		if cachedResp, ok := cache[rKey]; ok {
+			w.Header().Set("X-Cache", "HIT")
+			sendResp(w, cachedResp)
+			wg.RUnlock()
+			log.Printf("response took from cache. Elapsed in %dms\n", time.Since(t).Milliseconds())
+			return
+		}
+		wg.RUnlock()
+
+		// Sending request to origin
+		log.Printf("sending request to: %s", origin+r.URL.Path)
+
+		resp, err := http.Get(origin + r.URL.Path)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		// Send response
+		for key, values := range resp.Header {
+			for _, value := range values {
+				w.Header().Add(key, value)
+			}
+		}
+		w.Header().Set("X-Cache", "MISS")
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
+
+		// Cache response
+		wg.Lock()
+		cache[rKey] = newCacheResp(resp)
+		wg.Unlock()
+		log.Printf("response cached. Elapsed in %dms\n", time.Since(t).Milliseconds())
+	})
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+// sendResp sends a response to the client, copying the headers and body
+func sendResp(w http.ResponseWriter, ci *cacheItem) {
+	w.WriteHeader(ci.StatusCode)
+
+	for key, values := range ci.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+
+	if _, err := w.Write(ci.Body); err != nil {
+		log.Fatalf("error while writing response body: %v", err)
+	}
+}
+
+// prepareCacheKey creates a unique cache key for the request
+func prepareCacheKey(r *http.Request) string {
+	var keyBuilder strings.Builder
+
+	keyBuilder.WriteString(r.URL.Path)
+
+	for key, values := range r.Header {
+		_, err := keyBuilder.WriteString(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = keyBuilder.WriteString(strings.Join(values, ""))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalf("error reading request body: %v", err)
+	}
+	keyBuilder.Write(body)
+
+	r.Body = io.NopCloser(bytes.NewReader(body))
+
+	hash := sha256.New()
+	hash.Write([]byte(keyBuilder.String()))
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func newCacheResp(r *http.Response) *cacheItem {
+	ci := &cacheItem{
+		StatusCode: r.StatusCode,
+		Header:     make(http.Header),
+	}
+
+	for key, valuse := range r.Header {
+		for _, value := range valuse {
+			ci.Header.Add(key, value)
+		}
+	}
+	ci.Header.Add("X-Cache", "HIT")
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ci.Body = body
+
+	return ci
+}
